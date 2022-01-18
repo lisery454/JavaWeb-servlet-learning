986e356d9533b1a9375eacfb84cf429d06f4b10d
Lisery <12323451@we.asdf>
2022-01-18 17:03:42 +0800

    request

---
+++ request/src/main/java/com/lisery/servlet/LoginServlet.java
@@ -0,0 +1,41 @@
+package com.lisery.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.Arrays;
+
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("utf-8");
+        resp.setCharacterEncoding("utf-8");
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String[] hobbies = req.getParameterValues("hobbies");
+
+        System.out.println("======================================");
+        System.out.println(username);
+        System.out.println(password);
+        System.out.println(Arrays.toString(hobbies));
+        System.out.println("======================================");
+
+        //System.out.println("请求路径："+req.getContextPath());
+        //System.out.println("请求URL："+req.getRequestURI());
+
+        req.getRequestDispatcher("/success.jsp").forward(req,resp);
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
---
+++ request/src/main/webapp/index.jsp
@@ -0,0 +1,26 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>登录</title>
+</head>
+<body>
+<h1>登录</h1>
+
+<div style="text-align: center">
+    <form action="${pageContext.request.contextPath}/login" method="post">
+        <label>
+            用户名：<input type="text" name="username"><br>
+            密码：<input type="password" name="password"><br>
+            爱好：
+            <input type="checkbox" name="hobbies" value="女孩"> 女孩
+            <input type="checkbox" name="hobbies" value="代码"> 代码
+            <input type="checkbox" name="hobbies" value="唱歌"> 唱歌
+            <input type="checkbox" name="hobbies" value="电影"> 电影
+            <br>
+            <input type="submit">
+        </label>
+    </form>
+</div>
+
+</body>
+</html>
